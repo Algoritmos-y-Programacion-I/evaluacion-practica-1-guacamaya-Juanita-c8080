23de4e18a652d1f1b1222e4a07f2e15fb1812124
Juanita Daniela Carreno Pineda <1025884751@u.icesi.edu.co>
2025-03-28 12:55:46 -0500

    terminado

--- src/ui/Guacamaya.java
+++ src/ui/Guacamaya.java
@@ -9,6 +9,7 @@ public class Guacamaya {
     // Arreglos de precios y unidades para todo el programa
     public static double[] precios;
     public static int[] unidades;
+    public static int referencias;
 
     public static void main(String[] args) {
 
@@ -68,7 +69,7 @@ public class Guacamaya {
                 case 5:
                     System.out.println("\nDigite el limite minimo de ventas a analizar");
                     double limite = reader.nextDouble();
-                    System.out.println("\nDe las "+precios.length+" referencias vendidas en el dia, "+consultarReferenciasSobreLimite(limite)+" superaron el limite minimo de ventas de "+limite);
+                    System.out.println("\nDe las " + precios.length + " referencias vendidas en el dia, "+consultarReferenciasSobreLimite(limite)+" superaron el limite minimo de ventas de "+limite);
                     break;
                 case 6:
                     System.out.println("\nGracias por usar nuestros servicios!");
@@ -92,46 +93,118 @@ public class Guacamaya {
      * pre: Los arreglos precios y unidades deben estar declarados
      * pos: Los arreglos precios y unidades quedan inicializados
      */
+    
     public static void establecerCantidadVendida() {
 
         System.out.println("\nDigite el numero de referencias de producto diferentes vendidas en el dia ");
-        int referencias = reader.nextInt();
+        referencias = reader.nextInt();
 
         precios = new double[referencias];
         unidades = new int[referencias];
 
     }
 
+    /**
+     * Descripcion: Este metodo se encarga de preguntar al usuario los datos de los precios y las unidades de la tienda
+     * pre: El Scanner reader debe estar inicializado
+     * pre: Los arreglos precios y unidades deben estar declarados
+     * pos: Se guardan los diferentes datos dentro de sus arreglos correspondientes
+     * @param
+     * @return
+     */
     public static void solicitarDatos(){
 
-        
-     
+        for (int i = 0; i < precios.length; i++){
+            System.out.println("Ingrese el precio del producto " + (i + 1) + ": ");
+            precios [i] = reader.nextDouble();
+            
+            System.out.println("Ingrese la cantidad vendida del producto " + (i + 1) + ": ");
+            unidades [i] = reader.nextInt();
+        }
     }
 
+    /**
+     * Descripcion: Este metodo se encarga de calcular cuantas unidades se vendieron en el dia
+     * pre: El Scanner reader debe estar inicializado
+     * pre: Los arreglos precios y unidades deben estar declarados
+     * pos: Se suman las variables almacenadas en el array unidades
+     * @param 
+     * @return int total
+     */
     public static int calcularTotalUnidadesVendidas(){
+        int total = 0;
 
-        return 0;
+        for (int i = 0; i < unidades.length; i++){
+            total += unidades[i];
+        }
 
-
+        return total;
     }
 
+    /**
+     * Descripcion: Este metodo se encarga de calcular el promedio de los precios dados
+     * pre: El Scanner reader debe estar inicializado
+     * pre: Los arreglos precios y unidades deben estar declarados
+     * pos: Se suman las variables almacenadas en el array precios y se dividen entre el largo del array
+     * @param 
+     * @return int total
+     */
     public static double calcularPrecioPromedio(){
+        double totalPrecio = 0;
 
-        return 0;
+        for (int i = 0; i < precios.length; i++){
+            totalPrecio += precios [i]; 
+        }
 
+        double promedio = totalPrecio / precios.length;
 
+        return promedio;
     }
 
+    /**
+     * Descripcion: Este metodo se encarga de calcular las ventas totales del dia
+     * pre: El Scanner reader debe estar inicializado
+     * pre: Los arreglos precios y unidades deben estar declarados
+     * pos: Se suman las variables almacenadas en el array unidades y el array precios
+     * @param 
+     * @return double ventasTotales
+     */
     public static double calcularVentasTotales(){
+        double totalPrecio = 0;
+        double totalUnidad = 0;
 
-        return 0;
+        for (int i = 0; i < precios.length; i++){
+            totalPrecio += precios [i]; 
+        }
+
+        for (int i = 0; i < unidades.length; i++){
+            totalUnidad += unidades[i];
+        }
+
+        double ventasTotales = totalPrecio * totalUnidad;
+        return ventasTotales;
 
 
     }
 
+    /**
+     * Descripcion: Este metodo se encarga de verificar cuantas referemcias tuvieron un precio mayor al limite dado
+     * pre: El Scanner reader debe estar inicializado
+     * pre: Los arreglos precios y unidades deben estar declarados
+     * pos: Se suman las variables que cumplan con ser mayores que el limite dado
+     * @param 
+     * @return int count
+     */
     public static int consultarReferenciasSobreLimite(double limite){
+        int count = 0;
 
-        return 0;
+        for (int i = 0; i < precios.length; i++){
+            if (precios[i] * unidades[i] > limite) {
+            count ++;
+            }
+        }  
+
+        return count;
 
     }
 
